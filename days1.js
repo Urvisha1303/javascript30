9e4b34be99aae8f0666c311faeac1c1beef112c8
urvisha bambhroliya <bambhroliyaurvisha79@gmail.com>
2023-11-24 18:05:59 +0530

    days1

---
+++ days1.js
@@ -0,0 +1,63 @@
+
+// Q1. Q2. single line comment [short cut : CTRL + /]
+// Welcome to 30DaysOfJavaScript
+
+// Q3. [short cut : /* shift + alt + A */]
+/*Welcome to
+     30
+     DaysOfJavaScript*/
+
+//  Q4.
+
+var name = "urvisha"
+var b = true;
+var d = null;
+var c;
+console.log(typeof(name));
+console.log(typeof(b));
+console.log(typeof(d));
+console.log(typeof(c));
+
+/* terminal clear short cut : ctrl + L */
+// Q5.Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+console.log(typeof "urvisha");
+console.log(typeof 5);
+console.log(typeof null);
+console.log(typeof true);
+console.log(typeof undefined);
+
+// Q6 Declare four variables without assigning values
+
+/* var firstName = ;
+var lastname = ;
+var maritalStatus = ;
+var country = ;
+var age = ; */
+
+// Q7 Declare four variables with assigned values
+var firstName = 'urvisha';
+var lastName = 'donga';
+var maritalStatus = 'married';
+var country = 'india';
+var age = 26;
+// Q8 Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+console.log(firstName)
+console.log(lastName)
+console.log(maritalStatus)
+console.log(country)
+console.log(age)
+
+//Q9 Declare variables to store your first name, last name, marital status, country and age in a single line
+
+var firstName = 'urvisha', lastName = 'donga',maritalStatus = 'married',country = 'india', age = 26;
+console.log(firstName,lastName , maritalStatus, country , age)
+
+// Q10 Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+var myAge = 25;
+var yourAge = 30;
+
+console.log("I am " + myAge + " years old.");
+console.log("you are " + yourAge + " years old.");
